2cea1ac338fbf35b53a4fd1b097b6263dec43ee8
user <user@DESKTOP-M9PCNJN>
2022-12-26 16:17:13 +0900

    Spring04 Cafe 댓글기능 추가

--- Spring04_Final/src/main/webapp/WEB-INF/table.sql
+++ Spring04_Final/src/main/webapp/WEB-INF/table.sql
@@ -12,3 +12,21 @@ CREATE TABLE board_file(
 );
 
 CREATE SEQUENCE board_file_seq; 
+
+
+-- 댓글을 저장할 테이블
+CREATE TABLE board_cofe_comment(
+	num NUMBER PRIMARY KEY, -- 댓글의 글번호
+	writer VARCHAR2(100), --댓글 작성자의 아이디
+	content VARCHAR2(500), --댓글 내용
+	target_id VARCHAR2(100), --댓글의 대상자 아이디
+	ref_group NUMBER,
+	comment_group NUMBER,
+	deleted CHAR(3) DEFAULT 'no',
+	regdate DATE
+);
+
+-- 댓글의 글번호를 얻어낼 시퀀스
+CREATE SEQUENCE board_cafe_comment_seq;
+
+	

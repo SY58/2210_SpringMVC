fe0fe3eedc3346ed9148d8a5c511524e787323f3
user <user@DESKTOP-M9PCNJN>
2022-12-27 15:57:06 +0900

    Spring04에 Transaction 관리 예제 추가

---
+++ Spring04_Final/src/main/java/com/sy/spring04/exception/DeliveryException.java
@@ -0,0 +1,17 @@
+package com.sy.spring04.exception;
+
+import org.springframework.dao.DataAccessException;
+
+/*
+ * 트랜잭션에 영향을 주는 예외 클래스 만들기
+ * - DataAccessException 클래스를 상속받아서 만든다.
+ */
+
+//배송 관련 예외를 발생시키고 싶을 때 사용할 클래스
+public class DeliveryException extends DataAccessException{
+
+	public DeliveryException(String msg) {
+		super(msg);		
+	}
+
+}

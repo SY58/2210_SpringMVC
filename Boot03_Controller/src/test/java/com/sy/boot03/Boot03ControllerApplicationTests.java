4fe7d3e4ec6f3aa1dc53e8e5a92064ddc18d25d8
user <user@DESKTOP-M9PCNJN>
2022-12-28 18:28:56 +0900

    Boot03_Controller 프로젝트 추가

---
+++ Boot03_Controller/src/test/java/com/sy/boot03/Boot03ControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sy.boot03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot03ControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

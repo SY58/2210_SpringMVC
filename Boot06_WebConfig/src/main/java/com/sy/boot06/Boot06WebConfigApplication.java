69cb342b69ac74eadcb9721e2d5523be6c51bf7e
user <user@DESKTOP-M9PCNJN>
2022-12-29 12:21:14 +0900

    Boot06_WebConfig 프로젝트 추가

---
+++ Boot06_WebConfig/src/main/java/com/sy/boot06/Boot06WebConfigApplication.java
@@ -0,0 +1,13 @@
+package com.sy.boot06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot06WebConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot06WebConfigApplication.class, args);
+	}
+
+}

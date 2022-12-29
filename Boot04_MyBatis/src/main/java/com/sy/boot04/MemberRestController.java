9b4c17639b98839437c14585c40dd350518991a9
user <user@DESKTOP-M9PCNJN>
2022-12-29 10:05:42 +0900

    Boot04 TestMapper 예제추가

--- Boot04_MyBatis/src/main/java/com/sy/boot04/MemberRestController.java
+++ Boot04_MyBatis/src/main/java/com/sy/boot04/MemberRestController.java
@@ -9,11 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sy.boot04.member.dao.MemberDao;
 import com.sy.boot04.member.dao.MemberDto;
 
+//1. @RestController를 이용해서 Controller를 만들고
 @RestController
 public class MemberRestController {
 
 	@Autowired MemberDao dao;
 	
+	//2. 메소드에서 어떤 Data를 리턴하면 리턴되는 데이터가 클라이언트에게 바로 응답된다.
+	//   String은 내용 그대로 응답되고, dto, Map, List 등은 JSON 문자열로 변환되어서 응답된다. 
 	@GetMapping("/member/list")
 	public List<MemberDto> list(){
 		

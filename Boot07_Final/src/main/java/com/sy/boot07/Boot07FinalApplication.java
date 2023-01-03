e343e6414005d1a47976a9d03a098aabdd698d11
syCHOI <sychoi.me@gmail.com>
2023-01-03 10:22:47 +0900

    Boot07에 custom.properties 추가, Controller 필기추가

--- Boot07_Final/src/main/java/com/sy/boot07/Boot07FinalApplication.java
+++ Boot07_Final/src/main/java/com/sy/boot07/Boot07FinalApplication.java
@@ -2,7 +2,9 @@ package com.sy.boot07;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:custom.properties") //resources/custom.propeties 로딩하기
 @SpringBootApplication
 public class Boot07FinalApplication {
 
